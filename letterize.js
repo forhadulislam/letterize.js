1d9d4d3f3243e32110472d5ade98eb30923c63d7
Sadi <ronex.sadi@yahoo.com>
2016-04-04 09:35:38 +0000

    Updated Lettrize for selecting multiple elements

--- letterize.js
+++ letterize.js
@@ -1,33 +1,40 @@
 
 var letterize = function (elementSelector, textType, childType){
-	var theElement = document.querySelector(elementSelector);
-  var theElementText = theElement.textContent;
-  var theElementHTML = theElement.innerHTML;
-  var outputText = "";
-
-	if(textType == "lines"){
-  	var allLines = theElementHTML.split("<br>");
+    var theElement = document.querySelectorAll(elementSelector);
+    var elemLen = theElement.length;
+    console.log(elemLen);
     
-    for (var i = 0, len = allLines.length; i < len; i++) {
-    	var className = "line" + (i + 1);
-    	outputText += "<" + childType + " class='" + className + "'>" + allLines[i] + "<br /></" + childType + "> ";          
-    }
-  }else if(textType == "words"){
-  	
-    var words = theElementText.split(" ");
+    for(var a=0; a<elemLen; a++){
+        
+        var theElementText = theElement[a].textContent;
+        var theElementHTML = theElement[a].innerHTML;
+        var outputText = "";
     
-    for (var i = 0, len = words.length; i < len; i++) {
-    	var className = "word" + (i + 1);
-    	outputText += "<" + childType + " class='" + className + "'>" + words[i] + "</" + childType + "> ";          
+        if(textType == "lines"){
+            var allLines = theElementHTML.split("<br>");
+        
+            for (var i = 0, len = allLines.length; i < len; i++) {
+            	var className = "line" + (i + 1);
+            	outputText += "<" + childType + " class='" + className + "'>" + allLines[i] + "<br /></" + childType + "> ";          
+            }
+            
+        }else if(textType == "words"){
+          
+            var words = theElementText.split(" ");
+            for (var i = 0, len = words.length; i < len; i++) {
+            	var className = "word" + (i + 1);
+            	outputText += "<" + childType + " class='" + className + "'>" + words[i] + "</" + childType + "> ";          
+            }
+            outputText = outputText.trim();
+        }else{
+        
+          for (var i = 0, len = theElementText.length; i < len; i++) {
+            	var className = "letter" + (i + 1);
+            	outputText += "<" + childType + " class='" + className + "'>" + theElementText[i] + "</" + childType + ">";          
+            }
+        }
+        theElement[a].innerHTML = outputText;
     }
-    outputText = outputText.trim();
-  }else{
-  
-  	for (var i = 0, len = theElementText.length; i < len; i++) {
-    	var className = "letter" + (i + 1);
-    	outputText += "<" + childType + " class='" + className + "'>" + theElementText[i] + "</" + childType + ">";          
-    }
-  }	
-  theElement.innerHTML = outputText;
+    
 }
 
